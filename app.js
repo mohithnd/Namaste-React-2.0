eede9d04d1f74bdd2d4194489f09d7051223b3c0
Mohit Agrawal <mohitagrawalhnd2003@gmail.com>
2024-06-27 20:58:50 +0530

    Episode 2 - using parcel to bundle our project and react using npm instead of cdn

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 /* <div id="parent">
   <div id="child">
     <h1>I am an h1 tag</h1>
@@ -11,7 +14,7 @@
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h1", {}, "This is Namaste React"),
     React.createElement("h2", {}, "I am an h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
