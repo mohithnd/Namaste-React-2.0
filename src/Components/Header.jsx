3cd8272000f0155dc7e303da63fb44f5837250b0
Mohit Agrawal <mohitagrawalhnd2003@gmail.com>
2024-07-02 22:29:25 +0530

    Added Login And Logout Button In Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,9 @@
 import { CLOUDINARY_LOGO_PATH, CLOUDINARY_BASE_URL } from "../utils/constants";
+import { useState } from "react";
 
 const Header = () => {
+  const [btnName, setBtnName] = useState("Login");
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -15,6 +18,14 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className={btnName}
+            onClick={() => {
+              setBtnName((prev) => (prev == "Login" ? "Logout" : "Login"));
+            }}
+          >
+            {btnName}
+          </button>
         </ul>
       </div>
     </div>
