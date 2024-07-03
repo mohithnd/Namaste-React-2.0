799e20e42ff4949b10ca94a9895a7d525cbfed1a
Mohit Agrawal <mohitagrawalhnd2003@gmail.com>
2024-07-03 20:34:50 +0530

    Added Search Functionality

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -6,6 +6,7 @@ import Shimmer from "./Shimmer";
 const Body = () => {
   const [resCards, setResCards] = useState([]);
   const [filtered, setFiltered] = useState(false);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     fetchData();
@@ -22,6 +23,42 @@ const Body = () => {
   ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value.trim());
+            }}
+          />
+          <button
+            className="search-btn"
+            onClick={async () => {
+              if (searchText.length === 0) {
+                return;
+              }
+              if (filtered) {
+                await fetchData();
+                setFiltered((prevFiltered) => !prevFiltered);
+              }
+              const filteredCards = resCards.filter(
+                (card) =>
+                  card.resName
+                    .toLowerCase()
+                    .includes(searchText.toLowerCase()) ||
+                  card.cuisines.toLowerCase().includes(searchText.toLowerCase())
+              );
+              if (filteredCards.length > 0) {
+                setResCards(filteredCards);
+                setFiltered((prevFiltered) => !prevFiltered);
+              }
+              setSearchText("");
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
