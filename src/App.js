0b4a5ce38cc0babd677f660433bf6af32cd8d15a
Mohit Agrawal <mohitagrawalhnd2003@gmail.com>
2024-07-04 00:42:30 +0530

    Added Basic Routing For About, Contact and Error Page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./Components/Header";
 import Body from "./Components/Body";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Error from "./Components/Error";
 
 const AppLayout = () => {
   return (
@@ -14,4 +16,22 @@ const AppLayout = () => {
   );
 };
 
-root.render(<AppLayout />);
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<RouterProvider router={appRouter} />);
